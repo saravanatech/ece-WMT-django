878660322318f29f05f896324a3983b1c8e4f6c1
saravana tech <rsaravanatech@gmail.com>
2024-10-13 19:21:10 +0530

    type added in batch log

--- first_operation/models/batch_log.py
+++ first_operation/models/batch_log.py
@@ -7,6 +7,8 @@ class BatchLog(models.Model):
     batch = models.ForeignKey(Batch, on_delete=models.CASCADE, db_index=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=10, default="info")
+
         
     def __str__(self): 
         return str(self.log_message)
